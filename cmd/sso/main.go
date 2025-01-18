812e13e2ef16ef84451c4ea67a3b58d2737a40ea
dune6 <ilya.voznesensky@gmail.com>
2025-01-18 17:21:27 +0300

    Config logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"github.com/dune6/sso-auth/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Printf("%+v\n", cfg)
+	log := setUpLogger(cfg.Env)
+	log.Info("starting sso", slog.String("env", cfg.Env))
+}
+
+func setUpLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	}
+	return log
 }
